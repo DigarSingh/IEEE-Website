6afe023d0298960f19cfecf7bc9839dc26c6e55f
BYTE90 <digarsingh2004@gmail.com>
2025-05-18 12:37:51 +0530

    Update 2.0

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e096bae7f807deb3"; }
+/******/ 		__webpack_require__.h = function() { return "902d604523580c3a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
