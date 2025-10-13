86973e8b049e8d9fc4105ba1f809652dfaa29691
Arvind <150140351+Arvind05092005@users.noreply.github.com>
2025-10-13 14:32:06 +0530

    changedss

--- frontend/src/pages/round1-quiz.js
+++ frontend/src/pages/round1-quiz.js
@@ -398,7 +398,7 @@ export default function Round1Quiz() {
                     onClick={() => {
                       document.documentElement.requestFullscreen();
                       setShowWarning(false);//asdfas
-                    }}
+                    }}//asdfasd
                     className="px-6 py-2 ml-2 font-semibold text-white transition-colors bg-red-800 rounded-lg hover:bg-red-700"
                   >
                     <FaExpand className="inline mr-2" />
